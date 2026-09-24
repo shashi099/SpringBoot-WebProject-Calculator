007f11efbfb113ead1a03462ab4d41f126c83ae4
Shashi Singh <shashisingh25494@gmail.com>
2026-09-24 17:34:16 +0530

    Sending data to controller and accepting the data in servlet way

--- src/main/java/com/hypo/SpringWebProject/HomeController.java
+++ src/main/java/com/hypo/SpringWebProject/HomeController.java
@@ -1,5 +1,6 @@
 package com.hypo.SpringWebProject;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,4 +12,15 @@ public class HomeController {
         System.out.println("in home");
         return "index.jsp";
     }
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest req){
+        System.out.println("add Called");
+        int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+        int result = num1 + num2;
+        System.out.println(result);
+        return "result.jsp";
+    }
+
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -3,7 +3,16 @@
 <html>
     <body>
         <h2>
-            Hello JSP Page
+            Hello Calculator
         </h2>
+
+        <form action="add">
+        <Label for="num1">Enter 1st Number :</Label>
+        <input type="text" id="num1" name="num1"><br>
+        <Label for="num2">Enter 2nd Number :</label>
+        <input type="text" id="num2" name="num2"><br>
+        <input type="submit" value="Submit">
+        </form>
+
     </body>
 </html>
