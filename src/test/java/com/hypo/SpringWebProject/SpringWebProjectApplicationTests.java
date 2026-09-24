ce1d29dcde4e04c19c754a9d7bad110408c9bcba
Shashi Singh <shashisingh25494@gmail.com>
2026-09-24 17:16:22 +0530

    Start the web project using Spring Boot let's see what happen next??

---
+++ src/test/java/com/hypo/SpringWebProject/SpringWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hypo.SpringWebProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
