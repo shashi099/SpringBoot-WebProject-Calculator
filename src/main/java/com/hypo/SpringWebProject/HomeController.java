bb5cf9094039bd9c21b1b037165b8dae30f47a21
Shashi Singh <shashisingh25494@gmail.com>
2026-09-24 17:25:46 +0530

    Request Mapping and adding dependency for the jsp page

--- src/main/java/com/hypo/SpringWebProject/HomeController.java
+++ src/main/java/com/hypo/SpringWebProject/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+    @RequestMapping("/")
     public String home() {
         System.out.println("in home");
         return "index.jsp";
