d167a48b772239e94308f0ba4eb8c4a03cde096d
Shashi Singh <shashisingh25494@gmail.com>
2026-09-25 12:25:12 +0530

    Display data on result page

--- src/main/java/com/hypo/SpringWebProject/HomeController.java
+++ src/main/java/com/hypo/SpringWebProject/HomeController.java
@@ -1,6 +1,7 @@
 package com.hypo.SpringWebProject;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,12 +15,15 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(HttpServletRequest req){
+    public String add(HttpServletRequest req, HttpSession session){
         System.out.println("add Called");
         int num1 = Integer.parseInt(req.getParameter("num1"));
         int num2 = Integer.parseInt(req.getParameter("num2"));
         int result = num1 + num2;
         System.out.println(result);
+
+        session.setAttribute("result", result);
+
         return "result.jsp";
     }
 
---
+++ src/main/webapp/result.jsp
@@ -0,0 +1,11 @@
+<%@page language="java" %>
+
+<html>
+    <body>
+        <h2>
+            Result is : <%= session.getAttribute("result") %>
+        </h2>
+
+
+    </body>
+</html>
