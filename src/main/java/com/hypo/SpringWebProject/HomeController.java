359d072185ece6a1b4ff0d80568a5a9939d02695
Shashi Singh <shashisingh25494@gmail.com>
2026-09-25 22:03:43 +0530

    Handle the request data and sending the data to the jsp page using MODELANDVIEW

--- src/main/java/com/hypo/SpringWebProject/HomeController.java
+++ src/main/java/com/hypo/SpringWebProject/HomeController.java
@@ -6,24 +6,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String start() {
+    public String home() {
         System.out.println("in home");
         return "index";
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model){
+    public ModelAndView add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, ModelAndView mv){
         int result = num1 + num2;
         System.out.println(result);
-
-        model.addAttribute("result", result);
-
-        return "result";
+        mv.addObject("result", result);
+        mv.setViewName("result"); // mapping name of the jsp page result.jsp
+        return mv;
     }
 
 }
