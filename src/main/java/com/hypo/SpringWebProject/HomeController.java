7fcbbcb02a01aef3bb31ae0525763f02a3c426f4
Shashi Singh <shashisingh25494@gmail.com>
2026-09-25 12:46:17 +0530

    Instead of using HTTPSESSION now we are shifting the MODEL EXTRACT OUR DATA FROM THE CLIENT

--- src/main/java/com/hypo/SpringWebProject/HomeController.java
+++ src/main/java/com/hypo/SpringWebProject/HomeController.java
@@ -3,6 +3,7 @@ package com.hypo.SpringWebProject;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,12 +17,11 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, HttpSession session){
-        System.out.println("add Called");
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model){
         int result = num1 + num2;
         System.out.println(result);
 
-        session.setAttribute("result", result);
+        model.addAttribute("result", result);
 
         return "result.jsp";
     }
